793ae48d1a39daf06d05b8cceca0ccd8233c39cd
Noel Luna <noelluna@Noels-MacBook-Pro-2.local>
2022-01-15 14:00:02 +0000

    Final Website Design

--- react-website/src/components/Cards.js
+++ react-website/src/components/Cards.js
@@ -9,14 +9,14 @@ function Cards () {
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
-                        <CardItem src='images/img-9.jpg' text='explore the water fall' label='Adventure' path='/services' />
-                        <CardItem src='images/img-2.jpg' text='Travel to Island in a private cruise' label='Luxury' path='/services' />
+                        <CardItem src='images/img-11.jpg' text='Explore the waterfall' label='Adventure' path='/services' />
+                        <CardItem src='images/img-13.jpg' text='Visit Aztec Pyramid' label='Luxury' path='/services' />
 
                     </ul>
                     <ul className='cards__items'>
-                        <CardItem src='images/img-4.jpg' text='explore the water fall' label='Adventure' path='/services' />
-                        <CardItem src='images/img-3.jpg' text='Travel to Island in a private cruise' label='Luxury' path='/services' />
-                        <CardItem src='images/img-5.jpg' text='Travel to Island in a private cruise' label='Luxury' path='/services' />
+                        <CardItem src='images/img-10.jpg' text='Explore the Cenotes' label='Adventure' path='/services' />
+                        <CardItem src='images/img-12.jpg' text='Visit the beautiful beaches of Mexico' label='Leisure' path='/services' />
+                        <CardItem src='images/img-14.jpg' text='Explore the lakes' label='Adventure' path='/services' />
                     </ul>
                 </div>
             </div>
