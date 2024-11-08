c06bacf45c6041e568604c6fa31ff3d840041652
Noel Luna <noelluna@noels-mbp-2.lan>
2024-11-07 20:16:20 -0600

    Completed Code refactoring

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
-import Home from './components/pages/Home';
 import Footer from './components/Footer';
+import HeroSection from '../src/components/HeroSection';
+import Biography from '../src/components/Biography';
+import PhotoGallery from '../src/components/PhotoGallery';
 
 
 function App() {
@@ -11,10 +14,13 @@ function App() {
     <>
     <Router>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-      </Routes>
+      <HeroSection />
+      <Biography />
+      <PhotoGallery />
       <Footer />
+      {/* <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes> */}
     </Router>
     </>
   );
